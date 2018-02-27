37b77cee187afe241fb9e3aef835e0bcb7f491fd
Siraj Ahmed <blu.knite@gmail.com>
2018-02-26 19:07:20 -0800

    Added Circle as an available Shape

---
+++ src/main/scala/com/palebluespeck/scalessing/shapes/Circle.scala
@@ -0,0 +1,24 @@
+package com.palebluespeck.scalessing.shapes
+
+import com.palebluespeck.scalessing.ScalessingApp
+import com.palebluespeck.scalessing.common.Position
+import processing.core.PConstants
+
+class Circle(var center: Position, var radius: Float, override val app: ScalessingApp) extends Shape[Circle] {
+  override def self: Circle = this
+
+  def setCenter(position: Position): Circle = {
+    center = position
+    self
+  }
+
+  def setRadius(r: Float): Circle = {
+    radius = r
+    self
+  }
+
+  override def drawShape(): Unit = {
+    app.ellipseMode(PConstants.RADIUS)
+    app.ellipse(center.x, center.y, radius, radius)
+  }
+}
