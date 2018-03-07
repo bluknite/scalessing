a51a517bb50a8ac42e45cfdf28d181502e258be5
Siraj Ahmed <blu.knite@gmail.com>
2018-03-06 20:39:39 -0800

    Added rotation support for shapes

--- src/main/scala/com/palebluespeck/scalessing/shapes/Shape.scala
+++ src/main/scala/com/palebluespeck/scalessing/shapes/Shape.scala
@@ -8,6 +8,7 @@ trait Shape[T <: Shape[T]] {
   var fill: Color = NoColor
   var stroke: Color = NoColor
   var position: Position = Position(0, 0)
+  var angle: Float = 0
 
   def self: T
 
@@ -32,11 +33,23 @@ trait Shape[T <: Shape[T]] {
   }
   def +(position: Position): T = withPosition(this.position + position)
 
+  def withAngle(angle: Float): T = {
+    this.angle = angle
+    self
+  }
+  def rotate(a: Float): T = {
+    withAngle(this.angle + a)
+    self
+  }
+  def rotate(a: Double): T = rotate(a.toFloat)
+
   def draw(): Unit = {
     app.fill(fill)
     app.stroke(stroke)
     app.translate(position)
+    app.rotate(angle)
     drawShape()
+    app.rotate(-angle)
     app.translate(-position)
   }
 
